e0369bd64394cc5af94e617af10adc78b5523b92
PedroBiondi <133990628+PedroBiondi@users.noreply.github.com>
2023-05-24 11:20:10 -0300

    Add files via upload

---
+++ Perfil --2/script/script.js
@@ -0,0 +1,20 @@
+
+if (localStorage.getItem('theme') == 'dark') {
+    setDarkMode();
+      if (document.getElementById('checkbox').checked) {
+      localStorage.setItem('chekbox',true);
+    }
+  }
+  
+  function setDarkMode() {
+    let isDark = document.body.classList.toggle('darkmode');
+    if(isDark) {
+      setDarkMode.checked = true;
+      localStorage.setItem('theme', 'dark');
+      document.getElementById('checkbox').setAttribute('checked', 'checked');
+    }
+    else {
+      setDarkMode.checked = true;
+      localStorage.removeItem('theme', 'dark');
+    }
+  }
